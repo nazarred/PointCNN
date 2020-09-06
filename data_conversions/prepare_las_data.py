7731eeced476775b504493e54026b96557598768
Nazar Redko <rednaz1990@gmail.com>
2020-09-06 10:58:12 +0300

    cosmetic

--- data_conversions/prepare_las_data.py
+++ data_conversions/prepare_las_data.py
@@ -254,38 +254,40 @@ def main():
                                   os.listdir(os.path.join(root, split))
                                   if filename.endswith('.h5')]
 
-    train_h5 = split_filelists['train']
-    random.shuffle(train_h5)
-    train_list = os.path.join(root, 'train_data_files.txt')
-    logger.info('{}-Saving {}...'.format(datetime.now(), train_list))
-    with open(train_list, 'w') as filelist:
-        list_num = math.ceil(len(train_h5) / args.h5_num)
-        for list_idx in range(list_num):
-            train_list_i = os.path.join(root, 'filelists', 'train_files_g_%d.txt' % list_idx)
-            with open(train_list_i, 'w') as filelist_i:
-                for h5_idx in range(args.h5_num):
-                    filename_idx = list_idx * args.h5_num + h5_idx
-                    if filename_idx > len(train_h5) - 1:
-                        break
-                    filename_h5 = train_h5[filename_idx]
-                    filelist_i.write('../' + filename_h5)
-            for repeat_idx in range(args.repeat_num):
-                filelist.write('./filelists/train_files_g_%d.txt\n' % list_idx)
+    train_h5 = split_filelists.get('train')
+    if train_h5:
+        random.shuffle(train_h5)
+        train_list = os.path.join(root, 'train_data_files.txt')
+        logger.info('{}-Saving {}...'.format(datetime.now(), train_list))
+        with open(train_list, 'w') as filelist:
+            list_num = math.ceil(len(train_h5) / args.h5_num)
+            for list_idx in range(list_num):
+                train_list_i = os.path.join(root, 'filelists', 'train_files_g_%d.txt' % list_idx)
+                with open(train_list_i, 'w') as filelist_i:
+                    for h5_idx in range(args.h5_num):
+                        filename_idx = list_idx * args.h5_num + h5_idx
+                        if filename_idx > len(train_h5) - 1:
+                            break
+                        filename_h5 = train_h5[filename_idx]
+                        filelist_i.write('../' + filename_h5)
+                for repeat_idx in range(args.repeat_num):
+                    filelist.write('./filelists/train_files_g_%d.txt\n' % list_idx)
 
-    val_h5 = split_filelists['val']
-    val_list = os.path.join(root, 'val_data_files.txt')
-    logger.info('{}-Saving {}...'.format(datetime.now(), val_list))
-    with open(val_list, 'w') as filelist:
-        for filename_h5 in val_h5:
-            filelist.write(filename_h5)
+    val_h5 = split_filelists.get('val')
+    if val_h5:
+        val_list = os.path.join(root, 'val_data_files.txt')
+        logger.info('{}-Saving {}...'.format(datetime.now(), val_list))
+        with open(val_list, 'w') as filelist:
+            for filename_h5 in val_h5:
+                filelist.write(filename_h5)
 
-    test_h5 = split_filelists['test']
-    test_list = os.path.join(root, 'test_files.txt')
-    logger.info('{}-Saving {}...'.format(datetime.now(), test_list))
-    with open(test_list, 'w') as filelist:
-        for filename_h5 in test_h5:
-            filelist.write(filename_h5)
-
+    test_h5 = split_filelists.get('test')
+    if test_h5:
+        test_list = os.path.join(root, 'test_files.txt')
+        logger.info('{}-Saving {}...'.format(datetime.now(), test_list))
+        with open(test_list, 'w') as filelist:
+            for filename_h5 in test_h5:
+                filelist.write(filename_h5)
 
 
 if __name__ == '__main__':
